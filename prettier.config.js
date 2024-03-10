3d9f22fa7fb75b9cbdf3a4c99139357cfa783c18
avgmensch <124395199+avgmensch@users.noreply.github.com>
2024-03-10 21:49:36 +0100

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.js",
+  tabWidth: 2,
+  printWidth: 100,
+};
